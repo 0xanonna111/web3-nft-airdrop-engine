97abf51edcf9c800b048a7ad59c13e5a7205b11a
Anonna <136353692+0xanonna111@users.noreply.github.com>
2026-01-18 14:38:24 +0600

    Create user.registry.js

---
+++ user.registry.js
@@ -0,0 +1,9 @@
+export function loadUsers() {
+  console.log("Loading users...");
+
+  return [
+    { address: "0xUSER001", nftCount: 2 },
+    { address: "0xUSER002", nftCount: 0 },
+    { address: "0xUSER003", nftCount: 5 }
+  ];
+}
